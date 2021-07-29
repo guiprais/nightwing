419524226c5d0d37fe8f4f6eb10619d6b45ee9bf
Guilherme de Prais <guilherme.prais@gmail.com>
2021-07-28 23:21:11 -0300

    feat: add styled-components and global style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import GlobalStyles from './assets/styles/global';
+
 export function App() {
   return (
     <>
+      <GlobalStyles />
       <div>Hello Nightwing</div>
     </>
   );
---
+++ src/assets/styles/global.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+
+    @media (max-width: 720) {
+      font-size: 87.5%; //14px
+    }
+  }
+
+  body {
+    /* background: ${({ theme }) => theme.colors.background}; */
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: sans-serif;
+    font-weight: 400;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
